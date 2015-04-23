1543ab679c614a859eea39cce4023f892df38af1
Pedro Casaubon <xperiments@gmail.com>
2015-04-23 13:05:52 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # grunt-browser-mdbagde
+
+	Simple grunt task that generates Browser Compatibility Tables form json data.
+
+## Install
+
+	$> npm install grunt-browser-mdbagde --save-dev
+
+## Add it to your MD file
+
+To add the badge code to your MD, insert the next comment into your MD source:
+
+	<!--browser-badge-->
+
+## Configure and run task
+
+    grunt.loadNpmTasks('grunt-browser-mdbadge');
+
+	grunt.initConfig({
+		browser-badge":{
+			basic:{
+				src:'./README.md', // <-- No dest specified, src will be overwrite
+				browsers:'./browsers.json'
+			},
+			customDestination: {
+				src:'./README.md',
+				dest:'./README_badge.md', // <-- Specify a destination file
+				browsers:'./browsers.json'
+			},
+			inlineData: {
+				src:'./README.md',  // <-- No dest specified, src will be overwrite
+				browsers:{
+					"explorer" : { "12.0" : false, "8.0" : false, "9.0" : false },
+					"firefox" : { "10.0" : true, "11.0" : true, "12.0" : false, "13.0" : true, "nightly" : true },
+					"chrome" : { "14.0" : true, "15.0" : true, "16.0" : true, "canary" : true },
+					"safari" : { "5.0.5" : false, "5.1.0" : false, "5.1.1" : true },
+					"opera" : { "10.6" : false, "11.0" : false, "11.6" : false },
+					"iphone": { "6.0" : true },
+					"ipad": { "6.0" : true },
+					"android-chrome": { "20.0" : true },
+					"android-browser": { "4.2" : true }
+				}
+			}
+		}
+	});
---
+++ tasks/grunt-browser-mdbadge.js
@@ -0,0 +1,61 @@
+
+'use strict';
+module.exports = function (grunt) {
+    grunt.registerMultiTask('browser-badge', 'Log stuff.', function() {
+
+        grunt.log.writeln('Generating Mardown Browser Badge for file: ', this.filesSrc[0] );
+
+        var data = this.data.browsers;
+        var jsonObj = typeof data ==="string" ? grunt.file.readJSON(data):data;
+        var browsers = Object.keys( jsonObj );
+
+        var th = browsers.join('|');
+        var thr = browsers.map(function(){ return "---";}).join('|');
+
+        var valuesMap = {};
+        browsers.forEach(function(browser){
+            valuesMap[ browser ] = Object.keys( jsonObj[browser] ).map( function(browserVersion){
+                return ['<sub>', jsonObj[browser][browserVersion] ? '✅':'⛔' , browserVersion,'</sub>'].join(' ');
+            });
+        });
+
+        var maxRows=0;
+        var browserLength;
+        browsers.forEach(function(browser){
+            browserLength = valuesMap[ browser ].length ;
+            maxRows = maxRows < browserLength ? browserLength :maxRows;
+        });
+        var output = [ th, thr ];
+        var lines = [];
+        var slines = [];
+        for( var i=0; i<maxRows; i++){
+            lines[ i ] = [];
+
+            browsers.forEach(function(browser,x){
+
+                lines[i][x] = valuesMap[browser][i] || " ";
+
+            });
+            slines[i] = lines[ i ].join('|');
+
+
+        }
+        var table = output.concat(slines).join('\n');
+
+        var f = grunt.file.read(this.filesSrc);
+        var hasBrowserBadge = f.indexOf('<!--browser-badge-->')!=-1;
+        var hasCloseBadge = f.indexOf('<!--!browser-badge-->')!=-1;
+
+        if( hasBrowserBadge && !hasCloseBadge ){
+            f = f.replace('<!--browser-badge-->','<!--browser-badge-->\n'+table+'\n<!--!browser-badge-->\n');
+        }
+
+        if( hasBrowserBadge && hasCloseBadge ){
+            f = f.replace(/(<!--browser-badge-->)([\s\S]*)(<!--!browser-badge-->)/g,'$1\n'+table+'\n$3');
+        }
+
+        grunt.file.write(this.data.dest, f );
+
+
+    });
+};
